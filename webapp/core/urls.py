f27f8312d8d6933a86b1f9e4bfaf3a5794368a78
Rajan66 <rajanmaharjan042@gmail.com>
2025-06-04 22:12:41 +0545

    feat(jwt): setup access and refresh tokens

--- webapp/core/urls.py
+++ webapp/core/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
 
+api_prefix = "api/v1"
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("user/", include("_user.urls")),
+    path(f"{api_prefix}/user/", include("_user.urls")),
+    path(f"{api_prefix}/gatekeeper/", include("_gatekeeper.urls")),
 ]
