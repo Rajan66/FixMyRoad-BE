0f6342bdac2a6306972a46aa228b457b4d37493f
Rajan66 <rajanmaharjan042@gmail.com>
2025-06-11 22:58:18 +0545

    fix(script): fix token.sh

--- webapp/token.sh
+++ webapp/token.sh
@@ -1,9 +1,6 @@
-curl -X 'POST' \
+curl -X POST \
     'http://127.0.0.1:8000/api/v1/gatekeeper/token/' \
     -H 'accept: application/json' \
     -H 'Content-Type: application/json' \
     -H 'X-CSRFTOKEN: KtOuYZu2fgs2k8IH5h4kiZi4zHcO8UUPSVyd1OYFWVOGNhFbesTWkcrleXnNT39o' \
-    -d '{
-  "email": "rajanmaharjan042@gmail.com",
-  "password": "admin"
-}' | jq -r '.access' | xsel --clipboard --input
+    -d '{"email": "rajanmaharjan042@gmail.com", "password": "admin"}' | tee >(jq -r '.access' | xsel --clipboard --input)
