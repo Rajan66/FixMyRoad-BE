a4a383b1e01141e64df2cca334ed1d00fd90735b
Rajan66 <rajanmaharjan042@gmail.com>
2025-06-11 22:48:30 +0545

    feat(models): add report models and other apps

---
+++ webapp/report/models/report.py
@@ -0,0 +1,44 @@
+from _user.models import UserProfile
+from base.enums import ReportStatus
+from base.models import AbstractBaseModel
+from cluster.models import Cluster
+from django.db import models
+
+
+class PotholeReport(AbstractBaseModel):
+    profile = models.ForeignKey(
+        UserProfile,
+        on_delete=models.CASCADE,
+        related_name="reports",
+    )
+
+    # report doesn't need to belong to a cluster right away
+    cluster = models.ForeignKey(
+        Cluster,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="reports",
+    )
+
+    title = models.CharField(
+        max_length="255",
+    )
+
+    description = models.TextField(
+        null=True,
+        blank=True,
+    )
+
+    latitude = models.FloatField()
+
+    longitude = models.FloatField()
+
+    status = models.CharField(
+        max_length=20,
+        choices=ReportStatus.STATUS_CHOICES,
+        default="open",
+    )
+
+    def __str__(self):
+        return f"Report {self.title} by {self.profile.first_name}"
