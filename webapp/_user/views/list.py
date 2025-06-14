3d6e498b85f154414ce868f150d53ae62bbc9b74
Rajan66 <rajanmaharjan042@gmail.com>
2025-06-14 15:14:08 +0545

    feat(user): create views for profile api

--- webapp/_user/views/list.py
+++ webapp/_user/views/list.py
@@ -1,6 +1,15 @@
 from rest_framework.generics import ListAPIView
+from serializers._profile import ListUserProfileSerializer
 from serializers._user import ListUserSerializer
+
+from _user.models import User, UserProfile
 
 
 class ListUserView(ListAPIView):
     serializer_class = ListUserSerializer
+    queryset = User.objects.all()
+
+
+class ListUserProfileView(ListAPIView):
+    serializer_class = ListUserProfileSerializer
+    queryset = UserProfile.objects.all()
