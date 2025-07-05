09bb6d5f9ba20b180ec4f97e595db576ffef6c63
Rajan66 <rajanmaharjan042@gmail.com>
2025-07-06 00:35:32 +0545

    fix: role based report list

--- webapp/report/views/list.py
+++ webapp/report/views/list.py
@@ -1,5 +1,5 @@
 from rest_framework.generics import ListAPIView
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from serializers.report import ListReportSerializer
 
 from report.models import PotholeReport
@@ -7,5 +7,18 @@ from report.models import PotholeReport
 
 class ListReportView(ListAPIView):
     serializer_class = ListReportSerializer
-    queryset = PotholeReport.objects.all()
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if not user.is_authenticated or user.role == "user":
+            return PotholeReport.objects.all()
+
+        if user.role == "admin":
+            return PotholeReport.objects.all()
+
+        if user.role == "ward":
+            return PotholeReport.objects.filter(ward=user.profile.ward)
+
+        return PotholeReport.objects.all()
