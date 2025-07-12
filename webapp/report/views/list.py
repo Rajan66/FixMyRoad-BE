c2755884da627d9627d03cc6fbc83ae64387bf86
Rajan66 <rajanmaharjan042@gmail.com>
2025-07-12 07:59:11 +0545

    fix: nani

--- webapp/report/views/list.py
+++ webapp/report/views/list.py
@@ -13,6 +13,10 @@ class ListReportView(ListAPIView):
 
     def get_queryset(self):
         user = self.request.user
+        get_all_flag = self.request.query_params.get("getAll")
+
+        if get_all_flag:
+            return PotholeReport.objects.all().order_by("-updated_at")
 
         if not user.is_authenticated or user.role == "user":
             return PotholeReport.objects.all().order_by("-updated_at")
