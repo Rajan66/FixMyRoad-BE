cae44a7999143d5e584bb9ebf6f9087df4f6a86e
L3vi-Ackerman <l3vi3211@gmail.com>
2025-07-30 23:22:56 +0545

    add user list serializer in ward list

--- webapp/serializers/ward/list.py
+++ webapp/serializers/ward/list.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
+from serializers._user import ListUserSerializer
 
 from ward.models import Ward
 
 
 class ListWardSerializer(serializers.ModelSerializer):
+    user = ListUserSerializer()
+
     class Meta:
         model = Ward
         fields = "__all__"
