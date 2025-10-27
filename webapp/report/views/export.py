9327b7f5b34ca2bd7d01bda1d4cf653b92b91b9b
Rajan66 <rajanmaharjan042@gmail.com>
2025-10-28 01:04:19 +0545

    fix: remove yolo model

--- webapp/report/views/export.py
+++ webapp/report/views/export.py
@@ -1,13 +1,16 @@
 import csv
 
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse
+from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 
 from report.models import PotholeReport
 
 
 class ExportReportView(APIView):
-    def get(self, request):
+    permission_classes = [AllowAny]
+
+    def get(self, request: HttpRequest) -> HttpResponse:
         reports = PotholeReport.objects.filter(system_flag="valid")
 
         response = HttpResponse(content_type="text/csv")
@@ -28,16 +31,23 @@ class ExportReportView(APIView):
         )
 
         for report in reports:
+            ward_name = report.ward.name if report.ward and report.ward.name else ""  # noqa
+            ward_number = (
+                report.ward.ward_number
+                if report.ward and report.ward.ward_number
+                else ""
+            )
+
             writer.writerow(
                 [
-                    report.ward.name,
-                    report.ward.ward_number,
-                    report.title,
-                    report.description,
-                    report.latitude,
-                    report.longitude,
-                    report.status,
-                    report.severity,
+                    ward_name,
+                    ward_number,
+                    report.title or "",
+                    report.description or "",
+                    report.latitude or "",
+                    report.longitude or "",
+                    report.status or "",
+                    report.severity or "",
                 ]
             )
 
